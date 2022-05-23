9c21a6d32a43d61b2782ae2890c83261406010f8
McKayla Kleinschrodt <gh1880@wayne.edu>
2022-05-23 11:30:10 -0400

    React application shell
    The basic shell for our react application, features all the JS files we will need.
    Not functional, but organized!

---
+++ components/Packages.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Packages(){
+    return (
+        <section id="Packages">
+            <p>
+                Packages Page
+            </p>
+        </section>
+    )
+}
---
+++ components/Prices.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Prices(){
+    return (
+        <section id="Prices">
+            <p>
+                Prices Page
+            </p>
+        </section>
+    )
+}
