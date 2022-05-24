4333d6259a9abcac906118bb2ed2fed281cdf796
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-05-24 11:19:43 -0400

    Created Contact.js component

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Contact(){
+    return (
+        <section id="Contact">
+            <p>
+                Contact Us
+            </p>
+        </section>
+    )
+}
