d6a74490f531e37a5618624f3b8f8109b06a182a
JoeBomm <jrbomm@gmail.com>
2022-05-30 20:59:48 -0400

    Add react-media-gallery to Media.js, import css to App.css

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,4 +1,27 @@
 import React from "react";
+import ImageGallery from 'react-image-gallery';
+// https://www.npmjs.com/package/react-image-gallery
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
+class MyGallery extends React.Component {
+  render() {
+    return <ImageGallery items={images}/>;
+  }
+}
 
 export default function Media(){
     return (
@@ -6,6 +29,9 @@ export default function Media(){
             <p>
                 Media Page
             </p>
+            <MyGallery/>
         </section>
     )
 }
+
+// https://www.npmjs.com/package/react-media-gallery
