36efe8c29393206f94899530cbdbf98fa6e9a567
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-06-05 00:04:33 -0400

    created simpler html based form.

---
+++ components/TextForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function TextForm(){
+    return (
+        <div id="TextForm">
+            <form>
+                <label>Enter your song submission:
+                    <input type="text" />
+                </label>
+            </form>
+        </div>
+    )
+}
