f36cd63e3386651c1ede7fa13a0646826c2eac37
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-06-06 13:07:41 -0400

    html version of forms created under Songsubmission2 component.

---
+++ src/components/SongSubmission2.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from 'react';
+
+export default function SongSubmission2(){
+    const [textItem, setItem] = useState("");
+
+    return (
+        <section id="SongSubmission">
+            <p>
+                Song Submission
+            </p>
+        <form>
+            <label>Enter your submission:
+                <input
+                type="text" 
+                value={textItem}
+                onChange={(e) => setItem(e.target.value)}
+                />
+            </label>
+        </form>
+        </section>
+    )
+}
