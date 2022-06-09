7f4229bb3d2cf7c258b47d3a41b25ca6a88da456
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-06-09 13:31:52 -0400

    Testing on home page.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import SongSubmission2 from "./SongSubmission2";
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h2>HOME PAGE</h2>
+                <SongSubmission2 /> {/* This line must be deleted before merging into main branch. */}
             </div>
         );
     }
