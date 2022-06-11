b569c6ddffeef3898898651fe135b920ec920d93
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-06-11 18:54:18 -0400

    Separate components have been created for a text submission form and a calendar date form. These components are not implemented in the site, so you cannot see them, but I have tested them and confirmed that are functional.

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,47 +1,20 @@
-import React, { Component } from "react";
-import "./CalendarStyle.css";
+import React from "react";
+import { useState } from 'react';
 
-class Calendar extends Component {
-    render() {
-        return (
-            <section id="Calendar">
-                <h2>Calendar</h2>
+export default function Calendar(){
+    const [textItem, setItem] = useState("");
 
-                <div class="month">
-                <ul>
-                    <li class="prev">&#10094;</li>
-                    <li class="next">&#10095;</li>
-                    <li>August<br></br><span style="font-size:18px">2021</span></li>
-                </ul>
-                </div>
-
-                <ul class="weekdays">
-                <li>Mo</li>
-                <li>Tu</li>
-                <li>We</li>
-                <li>Th</li>
-                <li>Fr</li>
-                <li>Sa</li>
-                <li>Su</li>
-                </ul>
-
-                <ul class="days">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>7</li>
-                <li>8</li>
-                <li>9</li>
-                <li><span class="active">10</span></li>
-                <li>11</li>
-                ...etc
-                </ul> 
-            </section>
-        );
-    }
+    return (
+        <section id="Calendar">
+            <p>
+                Calendar
+            </p>
+        <form>
+            <label>Select a date:
+                <input type="date" id="Calendar_input"></input>
+                <input type="submit" value="Submit"></input>
+            </label>
+        </form>
+        </section>
+    )
 }
-
-export default Calendar;
--- src/components/SongSubmission.jsx
+++ src/components/SongSubmission.jsx
@@ -1,28 +1,24 @@
 import React from "react";
+import { useState } from 'react';
 
-class SongSubmission extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {    this.setState({value: event.target.value});  }
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-            <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+export default function SongSubmission2(){
+    const [textItem, setItem] = useState("");
+
+    return (
+        <section id="SongSubmission">
+            <p>
+                Song Submission
+            </p>
+        <form>
+            <label>Enter your submission:
+                <input
+                type="text" 
+                value={textItem}
+                onChange={(e) => setItem(e.target.value)}
+                />
+                <input type="submit" value="Submit"></input>
             </label>
-          <input type="submit" value="Submit" />
         </form>
-      );
-    }
-  }
+        </section>
+    )
+}
