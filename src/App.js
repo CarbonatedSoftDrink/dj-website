6259c3e3223a8a96d3e6d5635e85e9234e0d4533
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-07-13 19:38:34 -0400

    Revert "deleted services file"
    This reverts commit f0e18072db75c90d073ebdaa9b7f70635404f0f3.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Services from './components/Services';
 import Contact from './components/Contact';
 import Media from './components/Media';
 import Suggestions from './components/SongSubmission';
@@ -14,6 +15,7 @@ class App extends Component {
     return (
       <>
         <Home />
+        <Services />
         <Media />
         <Suggestions />
         <Contact />
