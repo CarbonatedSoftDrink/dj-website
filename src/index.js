d1d647bfb9b4daf26a0de13da788f1f4a789207e
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-07-14 01:19:49 -0400

    REVERRRRRT

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import cors from 'cors';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.use(cors());
+
 root.render(
   <React.StrictMode>
     <App />
