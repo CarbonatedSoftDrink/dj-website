b5e1ebf07111442153e30492a7701505af39b005
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-07-13 22:16:18 -0400

    installed cors and attempting fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+const cors = require("cors")
+App.use(
+  cors({
+    origin:"*",
+  })
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
