3b522ee62106c84a13ed00e83ae66b689894db0f
ysidro <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2022-07-15 22:05:15 -0400

    update import path

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-import sanityClient from "../client";
+import sanityClient from "../client.js";
 import { FaFileImage } from "react-icons/fa";
 
 export default function Services() {
