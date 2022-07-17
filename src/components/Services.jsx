8e040c4b0db7862c50893d69c985672764de3d2b
Maksuda19 <maksudaakhi@gmail.com>
2022-07-17 10:09:08 +0900

    Add

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -52,20 +52,7 @@ export default function Services() {
         </div>
       </section>
 
-      {/*<section id="Prices">
-        <h2>Prices</h2>
-        <p>DJ Only: $400.00 - $750.00 * May vary by location</p>
-        <p>
-          Fireworks: $200.0+ * Value depends on how many fireworks (2,4,6,8)
-        </p>
-        <p>Dry Ice: $150.00</p>
-        <p>Bogo Custom: $100.00</p>
-        <p>
-          Rental Speakers $250
-          <p>*Small System</p>
-          <p>*Large System</p>
-        </p>
-            </section>*/}
+      <Prices />
 
       <section id="Packages">
         <div class="container packages">
