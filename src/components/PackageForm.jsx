0be7f762ed9803405f0c2aa9634d6a5cd704e0aa
David Dettloff <dettloffd@gmail.com>
2022-07-19 10:18:58 -0400

    Began package inquiry form and modal to house it

---
+++ src/components/PackageForm.jsx
@@ -0,0 +1,162 @@
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
+export default function PackageForm(props) {
+  const [inputs, setInputs] = useState({
+    eventType: "",
+    eventSize: "",
+    eventDate: "",
+    contactName: "",
+    phoneNumber: "",
+    emailAddress: "",
+    additional: "",
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const sendFormPromise = sendForm();
+    toast.promise(
+      sendFormPromise,
+      {
+        loading: "Sending...",
+        success: "Your message has been sent, thank you!",
+      },
+      { loading: { position: "bottom-center" } },
+      { success: { duration: 8000, position: "bottom-center" } }
+    );
+
+    setTimeout(() => {
+      props.closeAfterSubmit();
+    }, 1500);
+  };
+
+  const sendForm = async () => {
+    try {
+      const fetchData = await fetch(
+        "https://formsubmit.co/ajax/dettloffdgmail.com",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            "Event Type": inputs.eventType,
+            "Event Size": inputs.partySize,
+            "Event Date": inputs.eventDate,
+            "Contact Name": inputs.contactName,
+            "Contact Number": inputs.phoneNumber,
+            "Contact Email": inputs.emailAddress,
+            "Additional Information": inputs.additional,
+          }),
+        }
+      );
+      setInputs({});
+
+      return fetchData;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="package-form-container">
+      <Toaster></Toaster>
+      <div className="package-form-inputs">
+        <form className="package-form" onSubmit={handleSubmit}>
+          <div className="package-form__group">
+            <label className="package-form__label">
+              Type of Event
+              </label>
+
+              <input
+                className="package-form__input"
+                type="text"
+                name="eventType"
+                value={inputs.eventType || ""}
+                onChange={handleChange}
+              />
+          </div>
+
+          <div className="package-form__group">
+            <label className="package-form__label">Estimated Event Size</label>
+            <input
+              className="package-form__input"
+              type="number"
+              name="eventSize"
+              value={inputs.eventSize || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="package-form__group">
+            <label className="package-form__label">Date (If Known)</label>
+            <input
+              className="package-form__input"
+              type="date"
+              name="eventDate"
+              value={inputs.eventDate || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="package-form__group">
+            <label className="package-form__label">Contact Name</label>
+            <input
+              className="package-form__input"
+              type="text"
+              name="contactName"
+              value={inputs.contactName || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="package-form__group">
+            <label className="package-form__label">Phone Number</label>
+            <input
+              className="package-form__input"
+              type="text"
+              name="phoneNumber"
+              value={inputs.phoneNumber || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="package-form__group">
+            <label className="package-form__label">Email Address</label>
+            <input
+              className="package-form__input"
+              type="email"
+              name="emailAddress"
+              value={inputs.emailAddress || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="package-form__group">
+            <label className="package-form__label">Additional Comments?</label>
+            <textarea
+              className="package-form__input"
+              type="textbox"
+              name="additional"
+              value={inputs.additional || ""}
+              onChange={handleChange}
+            />
+          </div>
+
+          <input
+            className="btn btn-primary package-form__button"
+            type="submit"
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
+import ModalBody from "react-bootstrap/ModalBody";
+import Modal from "react-bootstrap/Modal";
+import { Button } from "react-bootstrap";
+
 import { client } from "../client";
-import { FaFileImage } from "react-icons/fa";
 import DynamicIcon from "./DynamicIcon";
+import PackageForm from "./PackageForm";
 
 
 
 export default function Services() {
   const [services, setServices] = useState([]);
   const [packages, setPackages] = useState([]);
+  const [showModal, setShowModal] = useState(true);
+  
 
   useEffect(() => {
     const servicesQuery = '*[_type == "services"]';
@@ -34,21 +40,48 @@ export default function Services() {
     };
     fetchServicesData();
     fetchPackagesData();
-  }, []);  
+  }, []);
 
   return (
     <body>
       <section id="services">
+        <Modal
+          className="packages-modal"
+          onHide={() => setShowModal(false)}
+          size="lg"
+          show={showModal}
+          keyboard={true}
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header
+            closeVariant="white"
+            closeButton={() => setShowModal(false)}
+          >
+            <Modal.Title id="contained-modal-title-vcenter">
+              Modal heading
+            </Modal.Title>
+          </Modal.Header>
+          <ModalBody>
+            <PackageForm
+              closeAfterSubmit={() => setShowModal(false)}
+            ></PackageForm>
+          </ModalBody>
+          <Modal.Footer>
+            <Button onClick={() => setShowModal(false)}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
         <div class="container">
-
-
           <div class="container-fluid">
             <h1>Services</h1>
             <div class="row justify-content-center">
               {services?.map((service, index) => (
                 <div class="service-item card col-lg-4 col-md-6" key={index}>
                   <i className="services-img">
-                   <DynamicIcon iconData={service.icon ? service.icon : null } />
+                    <DynamicIcon
+                      iconData={service.icon ? service.icon : null}
+                    />
                   </i>
                   <h3 id="service-title">{service.service}</h3>
                   <p id="service-description">{service.description}</p>
