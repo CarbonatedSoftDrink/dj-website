519bc9a9a955f358a562c4220976de2341b1878f
David Dettloff <dettloffd@gmail.com>
2022-07-20 15:24:36 -0400

    Working on media section loading images

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,37 +1,75 @@
-import React from "react";
-import ImageGallery from 'react-image-gallery';
+import { React, useEffect, useState } from "react";
+import { client } from "../client";
+import ImageGallery from "react-image-gallery";
+import { urlFor } from "../client";
 // https://www.npmjs.com/package/react-image-gallery
 
-const images = [
-  {
-    original: 'https://libreshot.com/wp-content/uploads/2017/03/dancing-people-on-the-party-1536x1101.jpg',
-    thumbnail: 'https://libreshot.com/wp-content/uploads/2017/03/dancing-people-on-the-party-1536x1101.jpg',
-  },
-  {
-    original: 'http://www.publicdomainpictures.net/pictures/110000/velka/platterspieler-turntable.jpg',
-    thumbnail: 'http://www.publicdomainpictures.net/pictures/110000/velka/platterspieler-turntable.jpg',
-  },
-  {
-    original: 'https://images.pexels.com/photos/302879/pexels-photo-302879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    thumbnail: 'https://images.pexels.com/photos/302879/pexels-photo-302879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-  },
-];
+// const images = [
+//   {
+//     athing:
+//       "https://libreshot.com/wp-content/uploads/2017/03/dancing-people-on-the-party-1536x1101.jpg",
+//     thumbnail:
+//       "https://libreshot.com/wp-content/uploads/2017/03/dancing-people-on-the-party-1536x1101.jpg",
+//   },
+//   {
+//     original:
+//       "http://www.publicdomainpictures.net/pictures/110000/velka/platterspieler-turntable.jpg",
+//     thumbnail:
+//       "http://www.publicdomainpictures.net/pictures/110000/velka/platterspieler-turntable.jpg",
+//   },
+//   {
+//     original:
+//       "https://images.pexels.com/photos/302879/pexels-photo-302879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+//     thumbnail:
+//       "https://images.pexels.com/photos/302879/pexels-photo-302879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+//   },
+// ];
 
-class MyGallery extends React.Component {
-  render() {
-    return <ImageGallery items={images}/>;
-  }
-}
+// let images = [];
 
-export default function Media(){
-    return (
-        <section id="media">
-            <h1>
-                What We Can Do
-            </h1>
-            <MyGallery/>
-        </section>
-    )
+export default function Media() {
+  const [loadedImages, setLoadedImages] = useState([]);
+
+  useEffect(() => {
+    const imagesQuery = '*[_type == "images"]';
+
+    const fetchImages = async () => {
+      try {
+        const data = await client.fetch(imagesQuery);
+        let imageArray = [];
+        console.log(data);
+
+        data.forEach((item) => {
+          imageArray.push({
+            original: urlFor(item.image.asset),
+            thumbnail: urlFor(item.image.asset),
+            originalClass: "media-img-component"
+            // originalWidth: "500px",
+            // originalHeight: "300px"
+            // originalWidth: "100%",
+            // originalHeight: "100%"
+          });
+        });
+
+        setLoadedImages(imageArray);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchImages();
+  }, []);
+  
+
+  return (
+    <section id="media">
+      <h1>What We Can Do</h1>
+      <div className="media-gallery-container">
+
+      {loadedImages ? <ImageGallery items={loadedImages} /> : null}
+      </div>
+    </section>
+  );
 }
 
 // https://www.npmjs.com/package/react-media-gallery
