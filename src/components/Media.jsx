7b38ec9398349d41a0b5a9129debabc36416f3d0
David Dettloff <dettloffd@gmail.com>
2022-07-24 22:27:25 -0400

    Media gallery with uploaded pictures working

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -2,30 +2,6 @@ import { React, useEffect, useState } from "react";
 import { client } from "../client";
 import ImageGallery from "react-image-gallery";
 import { urlFor } from "../client";
-// https://www.npmjs.com/package/react-image-gallery
-
-// const images = [
-//   {
-//     athing:
-//       "https://libreshot.com/wp-content/uploads/2017/03/dancing-people-on-the-party-1536x1101.jpg",
-//     thumbnail:
-//       "https://libreshot.com/wp-content/uploads/2017/03/dancing-people-on-the-party-1536x1101.jpg",
-//   },
-//   {
-//     original:
-//       "http://www.publicdomainpictures.net/pictures/110000/velka/platterspieler-turntable.jpg",
-//     thumbnail:
-//       "http://www.publicdomainpictures.net/pictures/110000/velka/platterspieler-turntable.jpg",
-//   },
-//   {
-//     original:
-//       "https://images.pexels.com/photos/302879/pexels-photo-302879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-//     thumbnail:
-//       "https://images.pexels.com/photos/302879/pexels-photo-302879.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-//   },
-// ];
-
-// let images = [];
 
 export default function Media() {
   const [loadedImages, setLoadedImages] = useState([]);
@@ -44,10 +20,6 @@ export default function Media() {
             original: urlFor(item.image.asset),
             thumbnail: urlFor(item.image.asset),
             originalClass: "media-img-component"
-            // originalWidth: "500px",
-            // originalHeight: "300px"
-            // originalWidth: "100%",
-            // originalHeight: "100%"
           });
         });
 
@@ -62,13 +34,13 @@ export default function Media() {
   
 
   return (
-    <section id="media">
-      <h1>What We Can Do</h1>
+    <section id="media" style={{marginBottom: "5rem"}}>
+      <h1 style={{marginTop: "3rem", marginBottom: "3rem"}}>What We Can Do</h1>
       <div className="media-gallery-container">
 
       {loadedImages ? <ImageGallery items={loadedImages} /> : null}
       </div>
-    </section>
+    </section >
   );
 }
 
